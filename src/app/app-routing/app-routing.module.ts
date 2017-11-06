09f71c7949f3e318f4f8d6d3e8045de7943c4c0a
Ale Ksandar <88aleksandar88@gmail.com>
2017-11-06 22:02:24 +0100

    first commit

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,59 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AllGalleriesComponent } from '../components/all-galleries/all-galleries.component';
+import { CreateNewGalleryComponent } from '../components/create-new-gallery/create-new-gallery.component';
+import { LoginComponent } from '../components/login/login.component';
+import { MyGalleriesComponent } from '../components/my-galleries/my-galleries.component';
+import { RegisterComponent } from '../components/register/register.component';
+import { AuthGuard } from '../shared/guards/auth.guard';
+import { GuestGuard } from '../shared/guards/guest.guard';
+
+
+const appRoutes: Routes = [
+		{
+        path: '',
+        redirectTo: '/galleries',
+        pathMatch: 'full'
+    },
+    {
+        path: 'galleries',
+        component: AllGalleriesComponent,
+    },
+    {
+        path: 'my-galleries',
+        canActivate: [AuthGuard],
+        component: MyGalleriesComponent
+    },
+    {
+        path: 'create',
+        canActivate: [AuthGuard],
+        component: CreateNewGalleryComponent
+    },
+    {
+        path: 'login',
+        canActivate: [GuestGuard],
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        canActivate: [GuestGuard],
+        component: RegisterComponent
+    },
+    
+];
+
+
+@NgModule({
+	imports: [
+	CommonModule,
+	RouterModule.forRoot(
+		appRoutes
+		)
+	],
+	exports: [
+	RouterModule
+	],
+	declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AllGalleriesComponent } from './components/all-galleries/all-galleries.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MyGalleriesComponent } from './components/my-galleries/my-galleries.component';
+import { CreateNewGalleryComponent } from './components/create-new-gallery/create-new-gallery.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllGalleriesComponent,
+    LoginComponent,
+    RegisterComponent,
+    MyGalleriesComponent,
+    CreateNewGalleryComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
