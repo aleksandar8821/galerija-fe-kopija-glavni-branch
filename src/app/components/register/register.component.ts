174f6d05da23a72b515a4f8a7dfc63ea139c992f
Aleksandar Foltin <88aleksandar88@gmail.com>
2018-01-25 03:04:04 +0100

    proradio cli - samo uneo zeljenu verziju 1.6.5 u package.json i odradio npm install. takodje sada i backend vraca validacione greske na registraciji

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -27,7 +27,13 @@ export class RegisterComponent implements OnInit {
   	this.authService.register(this.user).subscribe(() => {
   		this.router.navigateByUrl('/login'); //ovo ti i ne biva okinuto (doduse i ne treba) jer funkcija na koju si se subscribeovao ne vraca nista preko o.next
   	}, (err: HttpErrorResponse) => {
-  		alert(`${err.error.error}`);
+      //sa for in iteracijom, nisam uspeo da dobavim ove greske, ova funkcija Object.values() je pravo cudo!
+      let errors = Object.values(err.error.errors) 
+      let errorString: string = ''
+      errors.forEach(function(message){
+        errorString += message + '\n'
+      });
+  		alert(errorString);
   	});
   }
 
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -60,6 +60,7 @@ export class AuthService {
         'accepted_terms': user.acceptedTerms
       }).subscribe((data: { token: string, logedUser: any }) => {
         window.localStorage.setItem('loginToken', data.token);
+        console.log(data)
         alert('You are now successfully registered')
         this.isAuthenticated = true;
         this.loggedUser = new User(data.logedUser.id, data.logedUser.first_name, data.logedUser.last_name, data.logedUser.email)
@@ -67,6 +68,7 @@ export class AuthService {
         window.localStorage.setItem('loggedUserNameFirstLetter', this.loggedUserNameFirstLetter)
         this.router.navigateByUrl('/');
       }, (err) => {
+        console.log(err)
         return o.error(err);
       });
     });
