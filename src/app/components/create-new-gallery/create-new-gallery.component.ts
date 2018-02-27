1dbdff0055dced1d2af2f14db3ee8c3b2512204f
Aleksandar Foltin <88aleksandar88@gmail.com>
2018-02-27 20:09:11 +0100

    validacija slike i jos neke dopune, downloadovan font awesome

--- src/app/components/create-new-gallery/create-new-gallery.component.ts
+++ src/app/components/create-new-gallery/create-new-gallery.component.ts
@@ -17,20 +17,32 @@ export class CreateNewGalleryComponent implements OnInit {
   public selectedImage: any
   public files : FileList; 
   public url: any
+  public uploadImageError: string
   
   getFiles(event){ 
       this.files = event.target.files;
-      // console.log(this.files[0]);
+      // console.log(event.target.files[0]);
       this.selectedImage = event.target.files[0]
+      this.uploadImageError = null
       // Na ovaj nacin prikazujem thumbnail nakon uploada fajla (prekopirano odavde: https://stackoverflow.com/questions/39074806/how-to-preview-picture-stored-in-the-fake-path-in-angular-2-typescript   isto imas i ovde: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded?rq=1)
       if (event.target.files && event.target.files[0]) {
+        if(!event.target.files[0].type.startsWith('image/')){
+          event.target.value = null //ovo je inace nacin da se iz input type file izbrise fajl koji je odabran. evo jos par nacina (slicnih, gotovo istih): https://stackoverflow.com/questions/41759704/how-to-clear-files-from-input-type-file-using-jquery-or-javascript , https://www.sitepoint.com/community/t/clear-input-type-file/257508/2 , https://nehalist.io/uploading-files-in-angular2/ <<< gledaj metodu clearFile() , https://stackoverflow.com/questions/40165271/how-to-reset-selected-file-with-input-tag-file-type-in-angular-2
+          this.url = null
+          this.uploadImageError = "File must be an image!"
+          return false //ovde moram izaci iz cele funkcije, da ne bi isao dalje, jer onda naravno izbacuje neke greske, jer sam prethodno izbrisao fajl iz input type file
+        }
+
         var reader = new FileReader();
 
         reader.onload = (event:any) => {
           this.url = event.target.result;
+          // console.log(event.target)
         }
 
         reader.readAsDataURL(event.target.files[0]);
+      }else{
+        this.url = null
       }     
   } 
 
